6ca0989a79d8d0472bfd607e91c43d3b1051b333
rr222cy <rr222cy@student.lnu.se>
2014-10-22 09:23:51 +0200

    Code cleanup, creation of class HitSoft17.cs

--- BlackJack/model/rules/InternationalNewGameStrategy.cs
+++ BlackJack/model/rules/InternationalNewGameStrategy.cs
@@ -7,7 +7,6 @@ namespace BlackJack.model.rules
 {
     class InternationalNewGameStrategy : NewGameStrategy
     {
-
         public override bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
             PlayerCard(a_deck, a_player);
--- BlackJack/view/IView.cs
+++ BlackJack/view/IView.cs
@@ -8,7 +8,6 @@ namespace BlackJack.view
     interface IView
     {
         void DisplayWelcomeMessage();
-        //int GetInput();
         void DisplayCard(model.Card a_card);
         void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score);
         void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score);
