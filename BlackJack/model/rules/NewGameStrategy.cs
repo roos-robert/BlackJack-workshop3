b00c4e15cc15f1dfed8e61b39581b0455bacd542
mk222xa <mk222xa@student.lnu.se>
2014-10-22 03:42:32 +0200

    DRY in Rules solution add

--- BlackJack/model/rules/NewGameStrategy.cs
+++ BlackJack/model/rules/NewGameStrategy.cs
@@ -8,21 +8,21 @@ namespace BlackJack.model.rules
     abstract class NewGameStrategy : INewGameStrategy
     {
         Card c;
-        public void PlayerCard(Deck a_deck, Dealer a_dealer, Player a_player)
+        public void PlayerCard(Deck a_deck, Player a_player)
         {            
             c = a_deck.GetCard();
             c.Show(true);
             a_player.DealCard(c);
         }
 
-        public void DealerCardShown(Deck a_deck, Dealer a_dealer, Player a_player)
+        public void DealerCardShown(Deck a_deck, Dealer a_dealer)
         {
             c = a_deck.GetCard();
             c.Show(true);
             a_dealer.DealCard(c);
         }
 
-        public void DealerCardHidden(Deck a_deck, Dealer a_dealer, Player a_player)
+        public void DealerCardHidden(Deck a_deck, Dealer a_dealer)
         {
             c = a_deck.GetCard();
             c.Show(false);
